0d347473a91d99805160d8eaaa2c7e317e959ebe
klarisapere5-debug <klarisapere5@gmail.com>
2026-06-01 10:04:19 -0500

    Fix styleUrl to styleUrls in app.ts

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   parkingSpots = [
