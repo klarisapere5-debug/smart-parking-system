fc37bd74fb0ff76e6172819e68688991977fb6ed
klarisapere5-debug <klarisapere5@gmail.com>
2026-06-01 10:25:03 -0500

    Refactor index.js for cleaner code structure

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 // Author: Klarisa Pere
 // HW4 Backend Server
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
 const parkingRoutes = require("./app_api/routes/parking");
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 mongoose.connect("mongodb://127.0.0.1/Loc8r")
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.log(err));
-
 app.use("/api/parking", parkingRoutes);
-
 app.get("/", (req, res) => {
     res.send("Klarisa HW4 Backend Running");
 });
-
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
